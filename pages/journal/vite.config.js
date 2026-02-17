beb4086b7f0e9634cc608da59be50b6c0fb3ef01
Xarnder <xanderwiles@gmail.com>
2026-02-17 14:36:00 +0000

    Added journal update

---
+++ pages/journal/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/pages/journal/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          editor: ['react-simplemde-editor', 'easymde', 'react-markdown'],
+        },
+      },
+    },
+  },
+})
